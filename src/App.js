67fe98c2d91a2cfc9e149e4afa538f70b1203ce9
yuraCOM <z0632655585@gmail.com>
2022-06-13 01:01:55 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 import WeatherDisplay from "./components/WeatherDisplay/WeatherDisplay";
 import "./bootstrap.min.css"
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
 
 // const startPlases = ["Kyiv", "Kharkiv", "Odessa", "Dnipro", "Zaporizhia", "Lviv"]
 
